a94051429c7feefab3ea8b5bdcf64f0cd41f871e
Julian Parra <julianparra@mail.ru>
2024-05-07 19:23:50 -0700

    Optimizado 100000%

--- src/main.c
+++ src/main.c
@@ -2,22 +2,34 @@
 #include <math.h>
 #include <stdio.h>
 
+#define MAX_SKY_RECTANGLES 10
+#define SKY_RECTANGLE_WIDTH 200
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
 int main(void) {
   // Initialization
   //--------------------------------------------------------------------------------------
-  const int screenWidth = 1080;
-  const int screenHeight = 720;
+  const int screenWidth = 800;
+  const int screenHeight = 450;
 
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-  InitWindow(screenWidth, screenHeight, "pajaros ENOJONES");
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+  InitWindow(screenWidth, screenHeight, "PAJAROS ENOJONES");
 
   float Xpos = 0.0;                      // Posicion de la bola en X
   float Ypos = (float)GetScreenHeight(); // Posicion de la bola en Y
-  float theta = 0;
-  float Vi = 0;       // Angulo del lanzamiento
+
+  float Vi = 50; // Velocidad inicial
+  //
+  const float deltaTheta = PI / 180;          // 10 deg
+  float theta = PI / 4;                       // Angulo de tiro
+  float sec2Theta = (1 / pow(cos(theta), 2)); // sec(theta)^2
+
+  float reciprocoVi2 = (1 / pow(Vi, 2)) * 4.9; // (1/vi^2) * 4.9
+  float tanTheta = tan(theta);                 // Tan(theta)
+  float cosTheta = cos(theta);                 // cos(theta)
+
   bool pause = false; // Bandera de pausa
 
   //--------------------------------------------------------------------------------------
@@ -28,32 +40,40 @@ int main(void) {
     // Update
     //----------------------------------------------------------------------------------
 
-    if (IsKeyPressed(KEY_SPACE))
+    if (IsKeyDown(KEY_SPACE))
       pause = !pause;
-    if (IsKeyPressed(KEY_UP)) {
-      printf("%f\n", theta);
-      theta += (PI / 18);
-    } else if (IsKeyPressed(KEY_DOWN)) {
-      theta -= (PI / 18);
-      printf("%f\n", theta);
+    if (IsKeyDown(KEY_UP)) {
+      theta += deltaTheta;
+      sec2Theta = (1 / pow(cos(theta), 2));
+      tanTheta = tan(theta);
+      cosTheta = cos(theta);
+      printf("Theta: %f\n", theta);
+    } else if (IsKeyDown(KEY_DOWN)) {
+      theta -= deltaTheta;
+      sec2Theta = (1 / pow(cos(theta), 2));
+      tanTheta = tan(theta);
+      cosTheta = cos(theta);
+      printf("Theta: %f\n", theta);
     }
-    if (IsKeyPressed(KEY_RIGHT)) {
-      printf("%f\n", Vi);
+    if (IsKeyDown(KEY_RIGHT)) {
+      printf("V: %f\n", Vi);
       Vi += 2.5;
-    } else if (IsKeyPressed(KEY_LEFT)) {
+      reciprocoVi2 = (1 / pow(Vi, 2)) * 4.9;
+    } else if (IsKeyDown(KEY_LEFT)) {
       Vi -= 2.5;
-      printf("%f\n", Vi);
+      printf("V: %f\n", Vi);
+      reciprocoVi2 = (1 / powf(Vi, 2)) * 4.9;
     }
 
     if (!pause) {
-      Xpos += Vi * cos(theta) * 20 *
-              GetFrameTime(); // We move at 200 pixels per second
-      Ypos =
-          -((tan(theta) * Xpos) - (4.9 * (1 / pow(Vi, 2)) *
-                                   (1 / pow(cos(theta), 2)) * pow(Xpos, 2))) +
-          GetScreenHeight();
-      if (Xpos >= GetScreenWidth())
+
+      Xpos += Vi * cosTheta * 20 * GetFrameTime();
+      Ypos = -((tanTheta * Xpos) - (pow(Xpos, 2) * reciprocoVi2 * sec2Theta)) +
+             (float)GetScreenHeight();
+
+      if (Xpos > GetScreenWidth()) {
         Xpos = 0;
+      }
       if (Ypos > GetScreenHeight()) {
         Ypos = GetScreenHeight();
         Xpos = 0;
@@ -66,10 +86,6 @@ int main(void) {
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
-
-    for (int i = 0; i < GetScreenWidth() / 100; i++)
-      DrawRectangle(200 * i, 0, 1, GetScreenHeight(), SKYBLUE);
-
     DrawCircle((int)Xpos, (int)Ypos, 10, RED);
 
     EndDrawing();
