978c1017c560ff8aafa75b3e85f0164d28c8fb68
Julian Parra <julianparra@mail.ru>
2024-05-10 01:18:22 -0700

    INCREIBLE OPTIMIZACION HACK EULER METODO LEAKED 2024

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 #include <math.h>
 #include <stdio.h>
 
@@ -20,73 +21,80 @@ int main(void) {
   float Xpos = 0.0;                      // Posicion de la bola en X
   float Ypos = (float)GetScreenHeight(); // Posicion de la bola en Y
 
+  float Vy = 0;
+  float Vx = 0;
   float Vi = 50; // Velocidad inicial
-  //
-  const float deltaTheta = PI / 180;          // 10 deg
-  float theta = PI / 4;                       // Angulo de tiro
-  float sec2Theta = (1 / pow(cos(theta), 2)); // sec(theta)^2
 
-  float reciprocoVi2 = (1 / pow(Vi, 2)) * 4.9; // (1/vi^2) * 4.9
-  float tanTheta = tan(theta);                 // Tan(theta)
-  float cosTheta = cos(theta);                 // cos(theta)
+  float theta = PI / 4; // Angulo de tiro
 
-  bool pause = false; // Bandera de pausa
+  bool pause = false;      // Bandera de pausa
+  bool disparando = false; // DISAPROOOOOOOO
 
+  Vector2 posInicial;
+  Vector2 posFinal;
+  Vector2 deltaPos = {0, 0};
   //--------------------------------------------------------------------------------------
-
-  // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
+  // Juego
+  while (!WindowShouldClose()) {
     // Update
     //----------------------------------------------------------------------------------
 
     if (IsKeyDown(KEY_SPACE))
       pause = !pause;
-    if (IsKeyDown(KEY_UP)) {
-      theta += deltaTheta;
-      sec2Theta = (1 / pow(cos(theta), 2));
-      tanTheta = tan(theta);
-      cosTheta = cos(theta);
-      printf("Theta: %f\n", theta);
-    } else if (IsKeyDown(KEY_DOWN)) {
-      theta -= deltaTheta;
-      sec2Theta = (1 / pow(cos(theta), 2));
-      tanTheta = tan(theta);
-      cosTheta = cos(theta);
-      printf("Theta: %f\n", theta);
-    }
-    if (IsKeyDown(KEY_RIGHT)) {
-      printf("V: %f\n", Vi);
-      Vi += 2.5;
-      reciprocoVi2 = (1 / pow(Vi, 2)) * 4.9;
-    } else if (IsKeyDown(KEY_LEFT)) {
-      Vi -= 2.5;
-      printf("V: %f\n", Vi);
-      reciprocoVi2 = (1 / powf(Vi, 2)) * 4.9;
-    }
 
     if (!pause) {
-
-      Xpos += Vi * cosTheta * 20 * GetFrameTime();
-      Ypos = -((tanTheta * Xpos) - (pow(Xpos, 2) * reciprocoVi2 * sec2Theta)) +
-             (float)GetScreenHeight();
-
-      if (Xpos > GetScreenWidth()) {
-        Xpos = 0;
-      }
-      if (Ypos > GetScreenHeight()) {
-        Ypos = GetScreenHeight();
-        Xpos = 0;
-      }
+      Xpos += Vx; // y = y_0 + dt*Vy
+      Ypos += Vy; // x = x_0 + dt*Vx_0
     }
 
+    Vy += 9.8 * GetFrameTime(); // + y''*dt para Vy
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !disparando) {
+      posInicial = GetMousePosition();
+      disparando = true;
+    } else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+      posFinal = GetMousePosition();
+      deltaPos = Vector2Subtract(posInicial, posFinal);
+      Xpos = posInicial.x;
+      Ypos = posInicial.y;
+
+      theta = -atan2f(deltaPos.y, deltaPos.x);
+
+      Vi = sqrt(deltaPos.x * deltaPos.x + deltaPos.y * deltaPos.y);
+
+      if (Vi > 70) {
+        Vi = 70;
+      }
+      ///////////////////////////////////////////////////////////////////////
+      // metodo de EULER oilar !!!!!!!!!!!!!!!!!!!!!!!
+      // (REF: https://www.physics.umd.edu/hep/drew/numerical_integration.html)
+      //
+      // el movimiento de proyectiles es descrito por increibles EDOs...
+      //
+      // y'(0) = V_y0 = v_0sin(theta)
+      // x'(0) = V_x0 = v_0cos(theta)
+      //
+      // x' = Vx = V_x0; CONST
+      // y' = Vy = V_x0 - g*dt (y'' = a_y = -g)
+      ///////////////////////////////////////////////////////////////////////
+      Vx = Vi * cos(theta) * 15 * GetFrameTime();
+      Vy = (-sin(theta) * Vi * 15 * GetFrameTime()); // Invertimos el signo pues el eje y esta invertido
+
+      printf("%f\n", theta);
+      printf("%f\n", Vi);
+      disparando = false;
+    }
     //----------------------------------------------------------------------------------
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
-    DrawCircle((int)Xpos, (int)Ypos, 10, RED);
+
+    if (!disparando && deltaPos.x != 0 && deltaPos.y != 0) {
+      DrawLineV(posInicial, posFinal, RED);
+      DrawCircle((int)Xpos, (int)Ypos, 20, RED);
+    }
 
     EndDrawing();
   }
